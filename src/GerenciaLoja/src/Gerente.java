382b77d6585a17aad8f4873f49593c7305d1d112
motacilio <marcosotacilio@discente.ufg.br>
2024-05-06 17:57:42 -0300

    aprimoração da classe gerente (versao 1.0 beta)

--- src/GerenciaLoja/src/Gerente.java
+++ src/GerenciaLoja/src/Gerente.java
@@ -17,6 +17,62 @@ public class Gerente extends Funcionario {
                 "3 - Remover Tabuleiro\n" +
                 "4 - Remover Digital\n");
 
-    }
+        Scanner ent = new Scanner(System.in);
+        int opcao = ent.nextInt();
 
+        if(opcao == 1){
+            System.out.println("Digite os dados do  jogo de tabuleiro");
+            System.out.println("Nome: ");
+            String nome = ent.nextLine();
+            System.out.println("Quantidade em estoque: ");
+            int quantidade = ent.nextInt();
+            System.out.println("Valor: ");
+            double valor = ent.nextDouble();
+            System.out.println("Genêro: ");
+            String genero = ent.nextLine();
+            System.out.println("Empresa: ");
+            String empresa = ent.nextLine();
+            System.out.println("Quantidade de Jogadores: ");
+            int qnt_jogadores = ent.nextInt();
+
+            Tabuleiro jogoTabNovo = new Tabuleiro(nome, quantidade, valor, genero, empresa, qnt_jogadores);
+            estoque.adicionarJogoTab(jogoTabNovo);
+        } else if(opcao == 2){
+            System.out.println("Digite os dados do  jogo digital");
+            System.out.println("Nome: ");
+            String nome = ent.nextLine();
+
+            System.out.println("Quantidade em estoque: ");
+            int quantidade = ent.nextInt();
+
+            System.out.println("Valor: ");
+            double valor = ent.nextDouble();
+
+            System.out.println("Genêro: ");
+            String genero = ent.nextLine();
+
+            System.out.println("Empresa: ");
+            String empresa = ent.nextLine();
+
+            System.out.println("Plataforma: ");
+            String plataforma = ent.nextLine();
+
+            System.out.println("Conectividade: ");
+            String conectividade = ent.nextLine();
+
+            Digital jogoDigNovo = new Digital(nome, quantidade, valor, genero,
+                    empresa, plataforma, conectividade);
+            estoque.adicionarJogoDig(jogoDigNovo);
+        } else if (opcao == 3){
+            System.out.println("Digite o codigo do jogo de tabuleiro: ");
+            int codigo = ent.nextInt();
+            estoque.removerJogoTab(codigo);
+        } else if(opcao == 4){
+            System.out.println("Digite o codigo do jogo digital");
+            int codigo = ent.nextInt();
+            estoque.removerJogoDig(codigo);
+        }
+
+        ent.close();
+    }
 }
